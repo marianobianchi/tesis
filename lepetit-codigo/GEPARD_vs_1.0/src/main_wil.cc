4bf892eb177cfabe43585a932da2711315064502
Mariano Bianchi <marianobianchi08+git@gmail.com>
2013-11-15 03:28:07 -0300

    GEPARD comentado

--- lepetit-codigo/GEPARD_vs_1.0/src/main_wil.cc
+++ lepetit-codigo/GEPARD_vs_1.0/src/main_wil.cc
@@ -43,8 +43,95 @@ void  draw_patch( IplImage * ap_image, CvMat * ap_image_points )
 	return;
 }
 
+
+void learn_pca_base(CvMat *lp_k,
+                    std::string l_base_name,
+                    cv::cv_pcabase &l_pcabase,
+                    int l_pat_size,
+                    int l_sup_size,
+                    int l_gep_nx,
+                    int l_gep_ny,
+                    int l_num_of_gep_trains,
+                    int l_num_of_pcas)
+{
+    std::cout << "Estamos aprendiendo para ud." << std::endl;
+    l_pcabase.set_parameters(lp_k,l_pat_size,l_sup_size,l_gep_nx,l_gep_ny,l_num_of_gep_trains);
+
+    int l_learn_counter = 0;
+
+    cv::cv_harris::set_radius(4);
+    cv::cv_harris::set_num_of_points(300);
+
+    for( int l_i=1; l_i<=10; ++l_i )
+    {
+        std::stringstream l_string;
+
+        l_string << "../base/img" << l_i << ".jpg";
+
+        IplImage * lp_color_image = cv::cv_load_image(l_string.str());
+        IplImage * lp_gray_image = cv::cv_convert_color_to_gray(lp_color_image);
+        IplImage * lp_image = cv::cv_smooth(lp_gray_image,5);
+        cvReleaseImage(&lp_color_image);
+        cvReleaseImage(&lp_gray_image);
+        cv::cv_show_image(lp_image);
+
+        CvMat * lp_points = cv::cv_harris::get_points(lp_image);
+
+        if( lp_points == NULL )
+        {
+            cvReleaseImage(&lp_image);
+
+            throw "error: lp_points is NULL!";
+        }
+        IplImage * lp_mask = cvCreateImage(cvGetSize(lp_image),IPL_DEPTH_8U,1);
+
+        cvSet(lp_mask,cvRealScalar(1));
+
+        for( int l_j=0; l_j<lp_points->cols; ++l_j )
+        {
+            int l_col = CV_MAT_ELEM(*lp_points,float,0,l_j);
+            int l_row = CV_MAT_ELEM(*lp_points,float,1,l_j);
+
+            if( l_col - l_pcabase.get_support_size()  > 0 &&
+                l_row - l_pcabase.get_support_size()  > 0 &&
+                l_col + l_pcabase.get_support_size()  < lp_image->width &&
+                l_row + l_pcabase.get_support_size()  < lp_image->height )
+            {
+                ++l_learn_counter;
+                l_pcabase.add( lp_image, lp_mask, l_row, l_col );
+            }
+        }
+        cvReleaseImage(&lp_image);
+        cvReleaseImage(&lp_mask);
+
+        std::cerr << l_learn_counter << " patches collected..." << std::endl;
+    }
+    std::cerr << "- start learning the pca basis (takes some minutes)..." << std::endl;
+
+    if( l_pcabase.learn_base(l_num_of_pcas) == false )
+    {
+        throw "error: learning the base!";
+
+    }
+
+    std::cerr << "- finished learning the base... "  << std::endl;
+    std::cerr << "try to save the base: ";
+
+    if( l_pcabase.save(l_base_name) == false )
+    {
+        std::cerr << "error: pca base could not be saved... " << std::endl;
+    }
+    else
+    {
+        std::cerr << " successfully saved..." << std::endl;
+    }
+}
+
+
+
 int main( int argc, char * argv[] )
 {
+    // Nombre del archivo en donde se guardo la base PCA
 	std::string l_base_name    = "../base/basis";
 
 	CvMat * lp_k = cvCreateMat(3,3,CV_32FC1);
@@ -72,90 +159,30 @@ int main( int argc, char * argv[] )
 	int l_pat_size			= 71;
 	int l_max_motion		= 23;
 
-	std::cerr << "try to load the pca basis:";
+	std::cerr << "try to load the pca basis: ";
 
-	if( l_pcabase.load(l_base_name) == false )
+	bool l_pcabase_was_loaded = l_pcabase.load(l_base_name);
+
+	if(  !l_pcabase_was_loaded )
 	{
-		std::cerr << " failed! " << std::endl;
+        std::cerr << "failed! " << std::endl;
 		std::cerr << "- a pca base is not available yet..." << std::endl;
 		std::cerr << "- we will learn one now for once and for all..." << std::endl;
 
-		l_pcabase.set_parameters(lp_k,l_pat_size,l_sup_size,l_gep_nx,l_gep_ny,l_num_of_gep_trains);
+        learn_pca_base( lp_k,
+                        l_base_name,
+                        l_pcabase,
+                        l_pat_size,
+                        l_sup_size,
+                        l_gep_nx,
+                        l_gep_ny,
+                        l_num_of_gep_trains,
+                        l_num_of_pcas);
 
-		int l_learn_counter = 0;
-
-		cv::cv_harris::set_radius(4);
-		cv::cv_harris::set_num_of_points(300);
-
-		for( int l_i=1; l_i<=10; ++l_i )
-		{
-			std::stringstream l_string;
-
-			l_string << "../base/img" << l_i << ".jpg";
-
-			IplImage * lp_color_image = cv::cv_load_image(l_string.str());
-			IplImage * lp_gray_image = cv::cv_convert_color_to_gray(lp_color_image);
-			IplImage * lp_image = cv::cv_smooth(lp_gray_image,5);
-			cvReleaseImage(&lp_color_image);
-			cvReleaseImage(&lp_gray_image);
-			cv::cv_show_image(lp_image);
-
-			CvMat * lp_points = cv::cv_harris::get_points(lp_image);
-
-			if( lp_points == NULL )
-			{
-				cvReleaseImage(&lp_image);
-
-				printf("error: lp_points is NULL!");
-				return 0;
-			}
-			IplImage * lp_mask = cvCreateImage(cvGetSize(lp_image),IPL_DEPTH_8U,1);
-
-			cvSet(lp_mask,cvRealScalar(1));
-
-			for( int l_j=0; l_j<lp_points->cols; ++l_j )
-			{
-				int l_col = CV_MAT_ELEM(*lp_points,float,0,l_j);
-				int l_row = CV_MAT_ELEM(*lp_points,float,1,l_j);
-
-				if( l_col - l_pcabase.get_support_size()  > 0 &&
-					l_row - l_pcabase.get_support_size()  > 0 &&
-					l_col + l_pcabase.get_support_size()  < lp_image->width &&
-					l_row + l_pcabase.get_support_size()  < lp_image->height )
-				{
-					++l_learn_counter;
-					l_pcabase.add( lp_image, lp_mask, l_row, l_col );
-				}
-			}
-			cvReleaseImage(&lp_image);
-			cvReleaseImage(&lp_mask);
-
-			std::cerr << l_learn_counter << " patches collected..." << std::endl;
-		}
-		std::cerr << "- start learning the pca basis (takes some minutes)..." << std::endl;
-
-		if( l_pcabase.learn_base(l_num_of_pcas) == false )
-		{
-			std::cerr << "error: learning the base!" << std::endl;
-			return 0;
-		}
-
-		std::cerr << "- finished learning the base... "  << std::endl;
-		std::cerr << "try to save the base: ";
-
-		if( l_pcabase.save(l_base_name) == false )
-		{
-			std::cerr << "error: pca base could not be saved... " << std::endl;
-		}
-		else
-		{
-			std::cerr << " successfully saved..." << std::endl;
-		}
 	}
-	else
-	{
-		std::cerr << "successfull!" << std::endl;
-	}
+
+    std::cerr << "PCA Base was " << (l_pcabase_was_loaded? "loaded": "lerned") << " successfully!" << std::endl;
+
 	std::vector<cv::cv_gepard*> l_gepard;
 	std::vector<cv::cv_hyper*>	l_hyper;
 	std::vector<cv::cv_esm*>    l_esm;
@@ -198,16 +225,19 @@ int main( int argc, char * argv[] )
 		IplImage * lp_gray  = NULL;
 		IplImage * lp_mean  = NULL;
 
+        // Obtiene una imagen de la camara
 		lp_color = l_camera.get_image();
+		// La transforma a escala de grises
 		lp_gray  = cv::cv_convert_color_to_gray(lp_color);
+		// Suaviza la imagen (escala de grises)
 		lp_mean	 = cv::cv_smooth(lp_gray,5);
 
 		int l_inlier=0;
 		int l_num_of_points=0;
 		int l_point_num=100;
 
+        // Obtiene keypoints en la imagen usando HARRIS
 		CvMat * lp_points = NULL;
-
 		cv::cv_harris::set_radius(4);
 		cv::cv_harris::set_num_of_points(l_point_num);
 		lp_points = cv::cv_harris::get_points(lp_mean);
@@ -222,11 +252,16 @@ int main( int argc, char * argv[] )
 			cvReleaseImage(&lp_color);
 			continue;
 		}
+
+        // ¿¿Guarda la cantidad de key points obtenidos??
 		l_num_of_points = lp_points->cols;
 
+		// Dibuja los keypoints en la imagen
 		cv::cv_draw_points(lp_color,lp_points,3,0,0,255,l_point_num);
 		cv::cv_draw_points(lp_color,lp_points,3,255,0,0,l_point_num/2);
 
+
+        // Obtiene la posicion del mouse en la imagen/ventana
 		int l_x=l_mouse.get_x();
 		int l_y=l_mouse.get_y();
 		int l_e=l_mouse.get_event();
@@ -236,6 +271,14 @@ int main( int argc, char * argv[] )
 			l_tra_col1 = l_x;
 			l_tra_row1 = l_y;
 		}
+
+
+		/*
+            Para cada key point se fija si está cerca de algún borde.
+            Si lo está, no hace nada con ese key point y pasa al siguiente.
+            Sino, se fija la ¿distancia cuadratica? del punto del mouse
+            a ese keypoint y si es menor a 10, dibuja el recuadro rojo y negro.
+		*/
 		for( int l_i=0; l_i<lp_points->cols; ++l_i )
 		{
 			int l_col = CV_MAT_ELEM(*lp_points,float,0,l_i);
@@ -275,12 +318,28 @@ int main( int argc, char * argv[] )
 				cvLine(lp_color,l_pt4,l_pt1,CV_RGB(255,0,0),1);
 			}
 		}
+		/* fin dibujo del recuadro rojo y negro */
+
+
+
+        /*
+            Este bloque toma nuevos ¿¿templates?? a partir de la posicion
+            del mouse a la hora de hacer click derecho y tomando como punto
+            de referencia el key point mas cercano al mouse.
+        */
+        // Si el mouse esta en la imagen y se hace click derecho:
 		if(  l_x != -1 && l_y != -1 && l_e == 2 )
 		{
 			float	l_best_distance = 10e10;
 			int		l_best_col=-1;
 			int		l_best_row=-1;
 
+			/*
+                Para cada key point se fija que este a cierta distancia de los bordes.
+                Si no cumple dicha distancia, continua.
+                Si la cumple, mira la ¿distancia cuadratica? entre el punto
+                y el mouse y se queda con el key point mas cercano al mouse
+            */
 			for( int l_i=0; l_i<lp_points->cols; ++l_i )
 			{
 				int l_col = CV_MAT_ELEM(*lp_points,float,0,l_i);
@@ -303,6 +362,8 @@ int main( int argc, char * argv[] )
 					l_best_col = l_col;
 				}
 			}
+
+            // Si se encontro un key point cercano al mouse
 			if( l_best_col != -1 && l_best_row != -1 )
 			{
 				//cv::cv_timer l_timer1;
@@ -320,17 +381,21 @@ int main( int argc, char * argv[] )
 				CV_MAT_ELEM(*lp_rec,float,0,3) = l_best_col-l_pat_size/2;
 				CV_MAT_ELEM(*lp_rec,float,1,3) = l_best_row+l_pat_size/2;
 
+                // Nuevo tracker para GEPARD ¿¿es un template??
 				cv::cv_gepard * lp_gepard = l_pcabase.get_tracker(lp_mean,599,l_best_row,l_best_col);
-				cv::cv_hyper * lp_hyper = new cv::cv_hyper;
-				cv::cv_esm * lp_esm = new cv::cv_esm;
 
+				// Nuevo tracker para HYPER ¿¿es un template??
+				cv::cv_hyper * lp_hyper = new cv::cv_hyper;
 				lp_hyper->set_parameters(10,10,4,l_max_motion,600,0);//23,300
 				lp_hyper->learn(lp_mean,lp_rec);
 
+                // Nuevo tracker para ESM ¿¿es un template??
+                cv::cv_esm * lp_esm = new cv::cv_esm;
 				lp_esm->learn(lp_mean,l_best_row-l_pat_size/2,l_best_col-l_pat_size/2,l_pat_size,l_pat_size);
 
 				cvReleaseMat(&lp_rec);
 
+                // Guarda el ¿¿template?? aprendido para cada tracker
 				if( lp_gepard != NULL && lp_hyper != NULL && lp_esm != NULL )
 				{
 					l_gepard.push_back(lp_gepard);
@@ -349,6 +414,9 @@ int main( int argc, char * argv[] )
 				l_learn_flag2 = true;
 			}
 		}
+
+
+        // Si se encontro un key point cercano al mouse
 		if( l_learn_flag1 == true )
 		{
 			cvReleaseImage(&lp_mean);
@@ -358,6 +426,8 @@ int main( int argc, char * argv[] )
 			l_learn_flag1 = false;
 			continue;
 		}
+
+
 		l_num_of_points = lp_points->cols;
 
 		CvMat * lp_best = cvCreateMat(3,4,CV_32FC1);
@@ -366,8 +436,15 @@ int main( int argc, char * argv[] )
 
 		std::stringstream l_data;
 
+
+        /*
+            REVISAR LO QUE SIGUE DE CODIGO....
+        */
+
+        // Para cada ¿¿template?? obtenido en CADA MODELO/TRACKER
 		for( int l_i=0; l_i<l_gepard.size(); ++l_i )
 		{
+            // Para cada key point
 			for( int l_j=0; l_j<lp_points->cols; ++l_j )
 			{
 				if( CV_MAT_ELEM(*lp_points,float,1,l_j)-l_pat_size/2-1 < 0 ||
@@ -382,12 +459,14 @@ int main( int argc, char * argv[] )
 
 				CvMat * lp_quad = NULL;
 
+                // Reconoce un patron ya aprendido en la nueva imagen
 				lp_quad = l_gepard[l_i]->recognize(	lp_mean,
 													CV_MAT_ELEM(*lp_points,float,1,l_j),
 													CV_MAT_ELEM(*lp_points,float,0,l_j) );
 				if( lp_quad == NULL )
 					continue;
 
+                // Sigue a ese patron en la imagen utilizando HYPER tracker
 				CvMat * lp_result = l_hyper[l_i]->track(lp_mean,lp_quad,3);
 
 				if( lp_result == NULL )
@@ -406,8 +485,14 @@ int main( int argc, char * argv[] )
 					cvReleaseMat(&lp_quad);
 					continue;
 				}
+
+                // Sigue a ese patron en la imagen utilizando ESM tracker
 				CvMat * lp_result1 = l_esm[l_i]->track(lp_mean,lp_result,10,1);
+
+				// Pisa el resultado de HYPER tracker con el de ESM tracker
 				cvCopy(lp_result1,lp_result);
+
+
 				cvReleaseMat(&lp_result1);
 
 				if( l_esm[l_i]->get_ncc() < l_esm_ncc )
@@ -424,6 +509,8 @@ int main( int argc, char * argv[] )
 				}
 				++l_inlier;
 
+                // Dibuja un cuadrado sobre la imagen en donde se encontro
+                // el patron (resultado de ESM tracker)
 				draw_patch(lp_color,lp_result);
 
 				cvReleaseMat(&lp_result);
@@ -436,6 +523,9 @@ int main( int argc, char * argv[] )
 		}
 		l_timer1.stop();
 
+
+		// Dibuja un texto que indica que se aprendio un nuevo ¿¿template?? (¿¿¿template == patch???)
+		// si se encontro un key point cercano al mouse
 		if( l_learn_flag2 == true )
 		{
 			CvFont l_font;
@@ -446,26 +536,33 @@ int main( int argc, char * argv[] )
 			l_learn_flag2 = false;
 			cvAddS(lp_color,cvScalar(0,0,255,0),lp_color);
 		}
+
+        // Dibuja la cantidad de FPS sobre la imagen
 		CvFont l_font;
 		std::stringstream l_stringstream;
 		cvInitFont(&l_font,CV_FONT_HERSHEY_DUPLEX,0.7,0.7);
 		l_stringstream << l_timer1.get_fps() << " fps";
 		cvPutText(lp_color,l_stringstream.str().c_str(),cvPoint(30,lp_color->height-30),&l_font,CV_RGB(0,255,0));
 
+        // Muestra la imagen a color en la ventana
 		cv::cv_show_image(lp_color,"runtime");
 
 		std::cout << "fps: " << l_timer1.get_fps() << " num_of_harris: " << l_num_of_points << " inlier: " << l_inlier << "/" << l_gepard.size() << char(13) << std::flush;
 
 		int l_key = cvWaitKey(1);
 
+        // Si apretas la o
 		if( l_key == 111 )
 		{
 			l_one_break = !l_one_break;
 		}
+
+		// Si apretas ESC
 		if( l_key == 27 )
 		{
 			break;
 		}
+        // Si apretas la d
 		if( l_key == 100 )
 		{
 			for( int l_i=0; l_i<l_gepard.size(); ++l_i )
