3ab04dfc102a52cdd85217c8264422a52ab69263
Mariano Bianchi <marianobianchi08+git@gmail.com>
2014-09-05 01:26:09 -0300

    Kdtree un poco más cerca... Falta hacerlo funcion

---
+++ codigo/cpp/kdtree/kdtree.h
@@ -0,0 +1,29 @@
+#ifndef __KDTREE__
+#define __KDTREE__
+
+
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+// Extra para el tutorial
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/io/pcd_io.h>
+
+
+typedef pcl::PointXYZ Point3D;
+typedef pcl::PointCloud<Point3D> PointCloud3D;
+
+
+
+
+
+
+
+
+#endif //__KDTREE__
