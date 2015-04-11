209169c94667bedb3f571fd9fcdf5c31edd6d45c
Mariano Bianchi <marianobianchi08+git@gmail.com>
2015-04-11 09:12:24 -0300

    Listo el abstract (faltan correcciones de pachi)

--- latex/count_abs_esp_words.py
+++ latex/count_abs_esp_words.py
@@ -2,17 +2,18 @@
 
 
 if __name__ == '__main__':
-    with open('abs_esp.tex', 'r') as f:
-        lines = f.readlines()
-        abs_line = ''
+    for fname in ['abs_esp.tex', 'abs_en.tex']:
+        with open(fname, 'r') as f:
+            lines = f.readlines()
+            abs_line = ''
 
-        for line in lines:
-            if line.startswith('\\noindent '):
-                abs_line = line
+            for line in lines:
+                if line.startswith('\\noindent '):
+                    abs_line = line
 
-        abs_line = abs_line[10:]
+            abs_line = abs_line[10:]
 
-        print(
-            "La cantidad de palabras del abstract es",
-            len(abs_line.split())
-        )
+            print(
+                "La cantidad de palabras del abstract {f} es".format(f=fname),
+                len(abs_line.split())
+            )
